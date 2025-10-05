4754dda6e58dee933c823c2fdfefbc953bf73a71
Mohamad <mohamad.badran@gmail.com>
2025-10-05 12:59:55 +0200

    collection config

---
+++ src/content.config.ts
@@ -0,0 +1,27 @@
+import { defineCollection, z } from "astro:content";
+import { file, glob } from "astro/loaders";
+
+const project = defineCollection({
+  loader: file("src/data/projects.json"),
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    image: z.string(),
+    slug: z.string(),
+    githubURL: z.string(),
+    liveURL: z.string(),
+  }),
+});
+
+const blog = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "src/data/blog" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.string().date(),
+    tags: z.array(z.string()),
+    image: z.string(),
+  }),
+});
+
+export const collections = { project, blog };
